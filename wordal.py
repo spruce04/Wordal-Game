175528e2ee43bae7e151a1a2b9b828697ca53d4c
Sam Flax <103819850+spruce04@users.noreply.github.com>
2022-04-19 12:31:08 +1000

    Update wordal.py

--- wordal.py
+++ wordal.py
@@ -14,49 +14,52 @@ for word in Fopen :
     word1 = word.upper()
     wordA = word1.split()
     WordBase = WordBase + wordA
+GuessCount = 6
 
 #Playing the game function
 def BackgroundSetup():
     global GuessCount
+    global ChosenWord
     #Choosing a random word. 
     ChosenNumber = random.randint(0,2497)
     ChosenWord = WordBase[ChosenNumber]
 
-    #Guess Box
-    GuessPrompt = Label(root, text = "Enter a Guess:")
-    GuessPrompt.grid(row = 3, column = 1)
-    GuessSpace = Entry(root, width = 20, borderwidth = 5)
-    GuessSpace.grid(row = 3, column = 2)
-    GuessInitiate = Button(root, text = 'Confirm Guess', borderwidth = 3, fg = 'blue', command = PlayWordal)
-    GuessInitiate.grid(row = 3, column = 3,)
-
     #Reset Guesses
     GuessCount = 6
-
-    #Import 5x5 Grid
-    for r in range(5):
-       for c in range(5):
-          Button(root, text = '?', borderwidth = 3, height = 4, width = 16,).grid(row = r+6,column = c)
+    Invalid_Guess.destroy()
    
 #Guesses.
 def PlayWordal() :
     global GuessCount
-    while GuessCount > 0:
-        PlayerGuess = input('Enter a Guess. NEED TO MAKE PROPER INPUT')
-        if len(PlayerGuess) != 5 :
-            print('Please enter a 5 letter word.')
-            continue
-        GuessCount = GuessCount-1
+    GuessCount = GuessCount-1
+    PlayerGuess = GuessSpace.get()  
+    if len(PlayerGuess) != 5 :
+        Invalid_Guess.grid(row = 3, column = 4)
+        GuessCount = GuessCount + 1
+    else :
+        print(PlayerGuess) 
+        print(GuessCount)   
         if GuessCount == 0 :
-            break
+            BackgroundSetup()
     
 #Importing GUI
 root.title("Wordal")
 greeting = Label(text = "Welcome to Wordal")
 greeting.grid(row = 1, column = 1)
-ButtonGuess = Button(root, text = 'Start/Reset', borderwidth = 3, height = 2, width = 10, command = BackgroundSetup)
+ButtonGuess = Button(root, text = 'Reset', borderwidth = 3, height = 2, width = 10, command = BackgroundSetup)
 ButtonGuess.grid(row = 1, column = 3)
-
+ #Guess Box
+GuessPrompt = Label(root, text = "Enter a Guess:")
+GuessPrompt.grid(row = 3, column = 1)
+GuessSpace = Entry(root, width = 20, borderwidth = 5)
+GuessSpace.grid(row = 3, column = 2)
+GuessInitiate = Button(root, text = 'Confirm Guess', borderwidth = 3, fg = 'blue', command = PlayWordal)
+GuessInitiate.grid(row = 3, column = 3,)
+Invalid_Guess = Label(root, text = "Enter a 5 Letter Word.", fg = 'red')
+#Import 5x5 Grid
+for r in range(6) :
+    for c in range(5) :
+        Button(root, text = '?', borderwidth = 3, height = 4, width = 16,).grid(row = r+6,column = c)
 root.mainloop()
 
 
